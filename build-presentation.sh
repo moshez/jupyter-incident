4111704187034bcd0a289213b68dad6f64d0b95d
Moshe Zadka <zadka.moshe@gmail.com>
2020-11-12 07:02:16 -0800

    temp commit

--- build-presentation.sh
+++ build-presentation.sh
@@ -2,12 +2,14 @@
 set -ex
 TALK=incident-management
 mkdir -p build
+cp $TALK.ipynb build/
+cd build
 jupyter nbconvert $TALK.ipynb --to markdown \
 	--TagRemovePreprocessor.remove_cell_tags='{"no_markdown"}'  \
 	--TagRemovePreprocessor.remove_all_outputs_tags='{"no_output"}'  \
-	--output build/$TALK.md
-pandoc --listings -o build/$TALK.tex build/$TALK.md 
-mf beamer --input build/$TALK.tex --outdir build
-(cd build && pdflatex talk.tex)
-(cd build && pdflatex handout.tex)
+	--output $TALK.md
+pandoc --listings -o $TALK.tex $TALK.md 
+mf beamer --input $TALK.tex --outdir .
+pdflatex talk.tex
+pdflatex handout.tex
 
